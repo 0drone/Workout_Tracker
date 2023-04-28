b099eb2e30956f1c769072fdec7a2591d26361cd
Eli <0drone@proton.me>
2023-04-28 18:19:58 -0500

    workout_tracker

---
+++ backend/models/userModel.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+   
+    minlength: 8
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+// static signup method
+userSchema.statics.signup = async function(email, password) {
+    //validation
+    if(!email || !password) {
+        throw Error('Email and password required')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Invalid email')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password must be at least 8 characters, contain at least one uppercase letter, one lowercase letter, one number, and one special character')
+    }
+
+  const exists = await this.findOne({ email })
+
+  if (exists) {
+    throw Error('Email already in use')
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+
+  const user = await this.create({ email, password: hash })
+
+  return user
+}
+
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email })
+    
+    if (!user) {
+        throw Error('Login Failed')
+    }
+    
+    const match = await bcrypt.compare(password, user.password)
+    
+    if (!match) {
+        throw Error('Login Failed')
+    }
+    
+    return user
+}
+
+module.exports = mongoose.model('User', userSchema)
